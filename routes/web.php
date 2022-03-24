ff7e873987f356443a17f324f4cf5a519a0fbd72
Amanda <amandaksailer@gmail.com>
2022-03-24 14:42:15 +0100

    files for mfscr with routes created -aks

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,16 @@
 <?php
 
+use App\Models\Header;
+use App\Models\Hero;
+use App\Models\About;
+use App\Models\Fact;
+use App\Models\Skill;
+use App\Models\Resume;
+use App\Models\Portfolio;
+use App\Models\Service;
+use App\Models\Testimonial;
+use App\Models\Contact;
+use App\Models\Footer;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +25,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $header = Header::first();
+    $hero = Hero::first();
+    $about = About::first();
+    $fact = Fact::first();
+    $skill = Skill::first();
+    $resume = Resume::first();
+    $portfolio = Portfolio::first();
+    $service = Service::first();
+    $testimonial = Testimonial::first();
+    $contact = Contact::first();
+    $footer = Footer::first();
+    return view('welcome', compact('header', 'hero', 'about', 'fact', 'skill', 'resume', 'portfolio', 'service', 'testimonial', 'contact', 'footer'));
 });
