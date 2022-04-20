aeb2c1c399595706f708335a1563008ea8bfcbca
Amanda <amandaksailer@gmail.com>
2022-04-20 17:03:57 +0200

    footer et al complete -aks

--- database/migrations/2022_03_24_133009_create_footers_table.php
+++ database/migrations/2022_03_24_133009_create_footers_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('footers', function (Blueprint $table) {
             $table->id();
+            $table->string('copyright');
             $table->timestamps();
         });
     }
--- database/seeders/FooterSeeder.php
+++ database/seeders/FooterSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class FooterSeeder extends Seeder
 {
@@ -14,6 +15,8 @@ class FooterSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('footers')->insert([
+            'copyright' => 'iPortfolio',
+        ]);
     }
 }
